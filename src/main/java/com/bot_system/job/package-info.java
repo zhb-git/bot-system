f3210074ad8c80fa97cb85cb2c951a40eca4847e
33101 <3310132142@qq.com>
2025-10-08 18:12:28 +0800

    1.0.0版本

---
+++ src/main/java/com/bot_system/job/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @className: package-info
+ * @author: Java之父
+ * @date: 2025/10/8 18:08
+ * @version: 1.0.0
+ * @description: 定时任务
+ */
+package com.bot_system.job;
---
+++ src/main/java/com/bot_system/model/pojo/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @className: package-info
+ * @author: Java之父
+ * @date: 2025/10/8 18:09
+ * @version: 1.0.0
+ * @description: 存放除开数据实体类（entity）、请求（request）、响应（response）的实体类
+ */
+package com.bot_system.model.pojo;
